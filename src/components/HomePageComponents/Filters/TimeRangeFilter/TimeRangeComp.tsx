38369c5fa5baec05be708fc152216171280ed668
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2024-02-05 20:45:16 +0530

    starting after long time commiting all unsaved code

--- src/components/HomePageComponents/Filters/TimeRangeFilter/TimeRangeComp.tsx
+++ src/components/HomePageComponents/Filters/TimeRangeFilter/TimeRangeComp.tsx
@@ -1,7 +1,9 @@
 import useTimeRangeState from "./useTimeRangeState";
+import moment from "moment";
 
 const TimeRangeComp = () => {
   const { onClickingOnTimeRange } = useTimeRangeState();
+  const date=new Date();
   return (
     <div className="d-flex flex-column p-0 m-2">
       <label className="dropdown-css">Time range </label>
@@ -9,13 +11,20 @@ const TimeRangeComp = () => {
       <select
         onChange={(e) => {
           onClickingOnTimeRange(e.currentTarget.value);
+          console.log(e.target.value)
           e.preventDefault();
         }}
       >
         <option value="">all time</option>
-        <option value="this_week">this week</option>
-        <option value="this_month">this month</option>
-        <option value="this_year">this year</option>
+        <option value={moment(date).startOf("week").format("YYYY-MM-DD")}>
+          this week
+        </option>
+        <option value={moment(date).startOf("month").format("YYYY-MM-DD")}>
+          this month
+        </option>
+        <option value={moment(date).startOf("year").format("YYYY-MM-DD")}>
+          this year
+        </option>
       </select>
     </div>
   );
